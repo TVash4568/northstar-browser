89fed0ddec7db8ecdce68ab6ed43b1b76679772d
TVash4568 <hwood4568@gmail.com>
2026-09-06 01:05:29 +0100

    Fix WebView2 renderer type alias

--- Windows/WebView2/WebView2RendererRegistry.cs
+++ Windows/WebView2/WebView2RendererRegistry.cs
@@ -1,21 +1,21 @@
-using Microsoft.Web.WebView2.Wpf;
 using Newton.Core.Domain;
+using WebViewControl = Microsoft.Web.WebView2.Wpf.WebView2;
 
 namespace NorthstarBrowser.Windows.WebView2;
 
 public sealed class WebView2RendererRegistry : IDisposable
 {
-    private readonly Dictionary<TabId, WebView2> _renderers = [];
-    public WebView2 Create(TabModel tab)
+    private readonly Dictionary<TabId, WebViewControl> _renderers = [];
+    public WebViewControl Create(TabModel tab)
     {
         if (_renderers.ContainsKey(tab.Id)) throw new InvalidOperationException("Tab already has a renderer.");
-        var renderer = new WebView2();
+        var renderer = new WebViewControl();
         _renderers.Add(tab.Id, renderer);
         tab.EngineInstanceId = new EngineInstanceId(Guid.NewGuid());
         return renderer;
     }
-    public WebView2 Get(TabModel tab) => _renderers[tab.Id];
-    public bool TryGet(TabModel tab, out WebView2 renderer) => _renderers.TryGetValue(tab.Id, out renderer!);
+    public WebViewControl Get(TabModel tab) => _renderers[tab.Id];
+    public bool TryGet(TabModel tab, out WebViewControl renderer) => _renderers.TryGetValue(tab.Id, out renderer!);
     public void Discard(TabModel tab)
     {
         if (_renderers.Remove(tab.Id, out var renderer)) renderer.Dispose();
